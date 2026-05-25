65da6e9df17a644b5c0e84f01f41c2cfeebb534e
SiddheshSoni <work.siddheshsoni@gmail.com>
2026-05-25 12:24:53 +0530

    e-commerce services task

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,13 +1,16 @@
+const productServices = require("../services/productServices");
+
 const getAllProducts =(req, res)=>{
     res.send("Fetched all Products!")
 }
 
 const getProductsById = (req, res)=>{
-    const id = req.params.id;
+    const id = productServices.selectProductsById(req.params.id);
     res.send(`Fetching product with ID: ${id}`);
 };
 
 const addProduct = (req, res)=>{
+    productServices.addProduct;
     res.send(` Added a new Product!`)
 };
 
---
+++ services/productServices.js
@@ -0,0 +1,17 @@
+
+
+const showAllProducts = ()=>{
+    return;
+}
+const selectProductsById = (id)=>{
+    return id;
+}
+const addProduct = ()=>{
+    return ;
+}
+
+module.exports ={
+    selectProductsById,
+    showAllProducts,
+    addProduct
+}
